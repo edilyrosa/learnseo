4dfd7b9dc373cfa120089516d90e813d771ee069
Edily Mora <edilyrosa@gmail.com>
2024-04-16 19:37:18 -0400

    starting project

--- app/page.tsx
+++ app/page.tsx
@@ -20,21 +20,24 @@ export default async function Index() {
   const isSupabaseConnected = canInitSupabaseClient();
 
   return (
-    <div className="flex-1 w-full flex flex-col gap-20 items-center">
-      <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-        <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
-          <DeployButton />
-          {isSupabaseConnected && <AuthButton />}
-        </div>
-      </nav>
 
-      <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
-        <Header />
-        <main className="flex-1 flex flex-col gap-6">
-          <h2 className="font-bold text-4xl mb-4">Next steps</h2>
-          {isSupabaseConnected ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
-        </main>
-      </div>
+    <>
+    <div className="flex-1 w-full flex flex-col gap-20 items-center">
+    <h1>Learn SEO</h1>
+    {/* //   <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
+    //     <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
+    //       <DeployButton />
+    //       {isSupabaseConnected && <AuthButton />}
+    //     </div>
+    //   </nav>
+
+    //   <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
+    //     <Header />
+    //     <main className="flex-1 flex flex-col gap-6">
+    //       <h2 className="font-bold text-4xl mb-4">Next steps</h2>
+    //       {isSupabaseConnected ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
+    //     </main>
+    //   </div> */}
 
       <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
         <p>
@@ -44,11 +47,12 @@ export default async function Index() {
             target="_blank"
             className="font-bold hover:underline"
             rel="noreferrer"
-          >
+            >
             Supabase
           </a>
         </p>
       </footer>
     </div>
+    </>
   );
 }

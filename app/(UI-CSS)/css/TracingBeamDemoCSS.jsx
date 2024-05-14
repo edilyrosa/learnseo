7de5b05a035edff9a365bb4002b15965a60bdd89
Edily Mora <edilyrosa@gmail.com>
2024-05-14 01:15:30 -0400

    fixing errors

--- app/(UI-CSS)/css/TracingBeamDemoCSS.jsx
+++ app/(UI-CSS)/css/TracingBeamDemoCSS.jsx
@@ -9,7 +9,7 @@ export default function TracingBeamDemo() {
     
     <TracingBeam className="px-6">
       <div className="max-w-2xl mx-auto antialiased pt-4 relative">
-        <h2>WHY LEARN SEO?</h2>
+        <h2>WHY LEARN CSS?</h2>
         {dummyContent.map((item, index) => (
           <div key={`content-${index}`} className="mb-10">
             <h2 className="bg-black text-white rounded-full text-sm w-fit px-4 py-1 mb-4">

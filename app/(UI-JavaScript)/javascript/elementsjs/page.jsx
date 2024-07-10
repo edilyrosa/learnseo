83e350af963eb75b00019cf7fc416657f74b85a6
Edily Mora <edilyrosa@gmail.com>
2024-07-10 12:25:43 -0400

    test 1

--- app/(UI-JavaScript)/javascript/elementsjs/page.jsx
+++ app/(UI-JavaScript)/javascript/elementsjs/page.jsx
@@ -1,68 +1,23 @@
-// import { createClient } from "@/utils/supabase/server";
-// import HeaderJS from "../../../header/HeaderJavaScript.jsx"
-// import TracingBeamBodyArticle from "../../../components/UI-Components/TracingBeamBodyArticle.jsx";
-// import '../globals.css'
-// export default async function Elements() {
-//   let news = [
-//     {
-//       title: '',
-//       body: '',
-//       badge:'',
-//       img_url:'',
-//       img_alt:'',
-//       keywords:'',
-//     }
-//   ];
-
-//   const supabase = createClient();
-
-//   try {
-//     const { data: news1 } = await supabase.from("articleJS_elementsHTML").select();
-//     if (news1) {
-//       news = news1;
-//     }
-//   } catch (error) {
-//     console.error("Error fetching data from Supabase:", error);
-//   }
-
-   
-//   return (
-//     <div>
-//              <HeaderJS/>
-//              <div style={{height:'4vh'}}></div>
-//              <div div className="view-sections">
-//               <TracingBeamBodyArticle news={news}/>
-//               </div>
-//           </div>
-//   );
-// }
-
 import { createClient } from "@/utils/supabase/server";
-import HeaderJS from "../../../header/HeaderJavaScript.jsx";
+import HeaderJS from "../../../header/HeaderJavaScript.jsx"
 import TracingBeamBodyArticle from "../../../components/UI-Components/TracingBeamBodyArticle.jsx";
-import '../globals.css';
-
-export async function getServerSideProps() {
-  const supabase = createClient();
-
+import '../globals.css'
+export default async function Elements() {
   let news = [
     {
       title: '',
       body: '',
-      badge: '',
-      img_url: '',
-      img_alt: '',
-      keywords: '',
+      badge:'',
+      img_url:'',
+      img_alt:'',
+      keywords:'',
     }
   ];
 
+  const supabase = createClient();
+
   try {
-    const { data: news1, error } = await supabase.from("articleJS_elementsHTML").select();
-
-    if (error) {
-      throw new Error(error.message);
-    }
-
+    const { data: news1 } = await supabase.from("articleJS_elementsHTML").select();
     if (news1) {
       news = news1;
     }
@@ -70,21 +25,16 @@ export async function getServerSideProps() {
     console.error("Error fetching data from Supabase:", error);
   }
 
-  return {
-    props: {
-      news,
-    },
-  };
-}
-
-export default function Elements({ news }) {
+   
   return (
     <div>
-      <HeaderJS />
-      <div style={{ height: '4vh' }}></div>
-      <div className="view-sections">
-        <TracingBeamBodyArticle news={news} />
-      </div>
-    </div>
+             <HeaderJS/>
+             <div style={{height:'4vh'}}></div>
+             <div div className="view-sections">
+              <TracingBeamBodyArticle news={news}/>
+              </div>
+          </div>
   );
 }
+
+

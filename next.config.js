830f9a40bfe3745c8d7c37f8ae3ac7b64128904c
Edily Mora <edilyrosa@gmail.com>
2024-12-20 13:35:10 -0400

    test 5

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const { withSentryConfig } = require('@sentry/nextjs');
 
 const nextConfig = {
+  reactStrictMode: true, // Habilita el modo estricto para desarrollo y producción
   images: {
     remotePatterns: [
       { hostname: 'firebasestorage.googleapis.com' },
@@ -17,11 +18,18 @@ const nextConfig = {
     }
     return config;
   },
-  hideSourceMaps: true,
-  disableLogger: true,
-  automaticVercelMonitors: true,
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  },
+  productionBrowserSourceMaps: false, // Para ocultar los mapas de origen en producción
+  sentry: {
+    hideSourceMaps: true, // Evita la exposición de mapas de origen en errores reportados
+    disableLogger: true, // Desactiva el registro en consola
+    automaticVercelMonitors: true,
+  },
 };
 
 module.exports = withSentryConfig(nextConfig, {
-  silent: true,
+  silent: true, // Reduce los logs de Sentry durante la compilación
 });
